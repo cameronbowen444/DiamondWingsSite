67aea4afa6587745ad645f013a6ae964cf6ab680
cameronbowen444 <cameronbowen555@gmail.com>
2026-06-13 16:04:01 -0700

    seo complete

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,30 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/about-us",
+        destination: "/#about",
+        permanent: true,
+      },
+      {
+        source: "/fleet",
+        destination: "/#fleet",
+        permanent: true,
+      },
+      {
+        source: "/reservations",
+        destination: "/#contact",
+        permanent: true,
+      },
+      {
+        source: "/contact-us",
+        destination: "/#contact",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
